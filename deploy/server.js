fed9f6c4b294aec7fa2ef9b227c0832e9292baa6
aliabbasnagari <aliabbasinayatali@gmail.com>
2025-05-12 02:13:23 +0500

    update

---
+++ deploy/server.js
@@ -0,0 +1,18 @@
+//require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+
+console.log("API URL:", process.env.REACT_APP_API_URL);
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
